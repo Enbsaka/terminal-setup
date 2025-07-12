822692f7748c925f735b8bb9d962d7fb80a4d701
Enbsaka <enzosakatauskas@gmail.com>
2025-07-12 20:10:00 -0300

    feat: add aliases personalizados (git, navegação, ls)

---
+++ custom/aliases.zsh
@@ -0,0 +1,27 @@
+# ==============================
+# Aliases personalizados
+# ==============================
+
+# --- Navegação ---
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias ~="cd ~"
+
+# --- Git (oh-my-zsh já tem vários, mas aqui vão extras) ---
+alias gs="git status"
+alias gc="git commit"
+alias gca="git commit --amend"
+alias gp="git push"
+alias gl="git pull"
+
+# --- Listagem (ls com opções) ---
+alias ls="ls --color=auto"
+alias ll="ls -la"
+alias la="ls -A"
+alias l="ls -CF"
+
+# --- Outros ---
+alias c="clear"
+alias h="history"
+alias zshrc="source ~/.zshrc"
